057e16eac2b883f5ffd867bbd771ed98a61f12e5
Erick Aytkulov <aytkulov@iac.spb.ru>
2019-10-24 11:49:27 +0300

    init commit

---
+++ src/main/java/ru/er/jpa2ddltemplate/Jpa2ddlTemplateApplication.java
@@ -0,0 +1,13 @@
+package ru.er.jpa2ddltemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa2ddlTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa2ddlTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/er/jpa2ddltemplate/Jpa2ddlTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.er.jpa2ddltemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2ddlTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
